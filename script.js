19f3b9551bb8e303e23b0b7d4f52ef3b59dabf73
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-12-07 01:52:17 -0800

    Added Geo data fetch function to fetch latitude and logitude of city

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var searchHistoryEl = $('#search-history')
 var searchInputEl = $('#search-value')
 var cityName = ""
 
+var apiKey = "f30afd0d9c78b669a42b1299a1eee959"
+
 var currentForecastEl = $('#current-forecast')
 var dailyForecastEl = $('#daily-forecast')
 
@@ -22,13 +24,47 @@ function formSubmit(event){
     }
 
     console.log(cityName)
-
+    geoData(cityName)
     // clears out search input after logging it into City Name variable
     searchInputEl.val("")
-    
+
 }
 
 
+// Fetch geolocation data (lat, lon)
+function geoData(city){
+    
+    var requestUrl=`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+
+    fetch(requestUrl)
+        .then(function(response){
+            console.log(response)
+            return response.json();
+        })
+        .then(function(data){
+            // we only have the data in this .then() menthod.
+            
+            // console.log(data)
+            console.log(data)
+            // working with the data we provide
+            
+            // 
+            // weatherData( /* provide lat and long data here*/ )
+
+        })
+        // Api parameters
+            // q=Name of city
+                // Name of the City
+                // Query strings start with ?
+                // Separate keys and value with equal sign =
+                // Separate key value pairs with ampersand &
+                // Looking for only city name
+        
+                // Save city name to history
+            // limit=5
+        
+            // appid={my custom api key}
+}
 
 // Render html elements to page
 
