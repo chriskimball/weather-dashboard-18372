dd695e4afebc5abe37d71c3af0a06a86d8c2803a
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-12-07 01:44:28 -0800

    Getting values from search form input or from search history button data attribute

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
+// Selecting elements to interact with
 var searchFormEl = $('#search-input')
 var searchHistoryEl = $('#search-history')
-console.log(searchFormEl)
-console.log(searchHistoryEl)
+var searchInputEl = $('#search-value')
+var cityName = ""
+
+var currentForecastEl = $('#current-forecast')
+var dailyForecastEl = $('#daily-forecast')
+
+// Fetch request functions
 
 function formSubmit(event){
+    
     event.preventDefault()
-    console.log(event.target)
-    console.log("can you see this?")
+    
+    cityName =""
+
+    if(event.type === "click"){
+        cityName = event.target.dataset.city
+    }else if (event.type === "submit"){
+        cityName = searchInputEl.val()
+    }
+
+    console.log(cityName)
+
+    // clears out search input after logging it into City Name variable
+    searchInputEl.val("")
+    
 }
 
+
+
+// Render html elements to page
+
+    // Render current day forecast
+
+    // Render 5 day forecast
+
+    // Add search history button
+
+
+// Event listeners
 searchFormEl.on('submit', formSubmit)
 
 searchHistoryEl.on('click', 'button', formSubmit)
